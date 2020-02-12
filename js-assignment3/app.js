8bd062c6afffafaf8c8908daff28a7354160fe57
Javad Sharifzadeh-Najafi <jsharifzadehnajafi@gmail.com>
2020-02-12 14:55:53 -0700

    Formatting
    Prettier formatting

--- js-assignment3/app.js
+++ js-assignment3/app.js
@@ -1,59 +1,46 @@
 // This area is a placeholder for fetch-data; each fetch will later be sent to individual script files in the future
 
-const characters = 'https://swapi.co/api/people/'
-let character = []
+const characters = 'https://swapi.co/api/people/';
+let character = [];
 
-const species = 'https://swapi.co/api/species/'
-let specie = []
+const species = 'https://swapi.co/api/species/';
+let specie = [];
 
-const vehicles = 'https://swapi.co/api/vehicles/'
-let vehicle = []
+const vehicles = 'https://swapi.co/api/vehicles/';
+let vehicle = [];
 
-const starships = 'https://swapi.co/api/starships/'
-let starship = []
+const starships = 'https://swapi.co/api/starships/';
+let starship = [];
 
-const planets = 'https://swapi.co/api/planets/'
-let planet = []
+const planets = 'https://swapi.co/api/planets/';
+let planet = [];
 
-
-
-
-fetch(characters)
-    .then(
-        function(a) {
-            a.json().then(function(data) {
-                character = data.results;
-    }); 
+fetch(characters).then(function(a) {
+    a.json().then(function(data) {
+        character = data.results;
+    });
 });
 
-fetch(species)
-    .then(
-        function(a) {
-            a.json().then(function(data) {
-                specie = data.results;
-    }); 
+fetch(species).then(function(a) {
+    a.json().then(function(data) {
+        specie = data.results;
+    });
 });
 
-fetch(vehicles)
-    .then(
-        function(a) {
-            a.json().then(function(data) {
-                vehicle = data.results;
-    }); 
+fetch(vehicles).then(function(a) {
+    a.json().then(function(data) {
+        vehicle = data.results;
+    });
 });
 
-fetch(starships)
-    .then(
-        function(a) {
-            a.json().then(function(data) {
-                starship = data.results;
-    }); 
+fetch(starships).then(function(a) {
+    a.json().then(function(data) {
+        starship = data.results;
+    });
 });
 
-fetch(planets)
-    .then(
-        function(a) {
-            a.json().then(function(data) {
-                planet = data.results;
-    }); 
+fetch(planets).then(function(a) {
+    a.json().then(function(data) {
+        planet = data.results;
+    });
 });
